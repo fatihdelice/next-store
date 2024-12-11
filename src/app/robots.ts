aca70d0acdab0f7a1f2062bd1632cc54b16724ec
fatihdelice <delicex58@gmail.com>
2024-12-11 16:31:52 +0300

    feat: robots.ts fix

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,10 @@
-export default function Robots() {
-    return `
-      User-agent: *
-      Disallow:
-      Sitemap: /sitemap.xml
-    `;
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Content-Type', 'text/plain');
+  res.send(`
+    User-agent: *
+    Disallow:
+    Sitemap: /sitemap.xml
+  `);
 }
